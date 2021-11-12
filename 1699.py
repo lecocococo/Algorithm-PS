042fff66dea3ffda0db7bb22c8ffa607f3390f17
LCM <62368629+lecocococo@users.noreply.github.com>
2021-11-13 04:17:52 +0900

    Create 1699.py

---
+++ 1699.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+dp = [i for i in range(n+1)]
+for i in range(1, n+1):
+    j = 1
+    while j*j <= i:
+        if dp[i] > dp[i - j * j] + 1:
+            dp[i] = dp[i - j * j] + 1
+        j += 1
+print(dp[n])
