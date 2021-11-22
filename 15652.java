d79add233e376c5d33cae011baa77b3a5a48826b
LCM <62368629+lecocococo@users.noreply.github.com>
2021-11-22 23:01:51 +0900

    Create 15652.java

---
+++ 15652.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static int[] arr;
+    public static boolean[] visited;
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        arr = new int[M];
+        visited = new boolean[N];
+        dfs(N, M,0);
+        System.out.println(sb);
+
+    }
+
+    public static void dfs(int N, int M, int depth) {
+        if (depth == M) {
+            for (int val : arr) {
+                sb.append(val).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (depth == 0){
+                arr[depth] = i + 1;
+                dfs(N, M, depth + 1);
+            }
+            else if(depth > 0 && i > arr[depth - 1] - 2){
+                arr[depth] = i + 1;
+                dfs(N, M, depth + 1);
+            }
+        }
+    }
+
+}
